cc0d65c2f76a26d4e3631e79e8224bf7f7ca9f2c
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-03-07 22:33:53 +0800

    created root reducer

--- src/config/store/store.ts
+++ src/config/store/store.ts
@@ -1,13 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 import { threadsSlice } from '@/features/threads/slices';
 import { authenticationSlice } from '@/features/authentication/slices';
 
+const rootReducer = combineReducers({
+  authentication: authenticationSlice,
+  threads: threadsSlice,
+});
+
 const store = configureStore({
-  reducer: {
-    authentication: authenticationSlice,
-    threads: threadsSlice,
-  },
+  reducer: rootReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
