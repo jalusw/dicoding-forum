7614c1511c2e4887789d1a916813dc0b388a4a26
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-03-09 09:26:26 +0800

    add navbar, and thread list

--- src/features/threads/components/home/HomePage.tsx
+++ src/features/threads/components/home/HomePage.tsx
@@ -1,7 +1,20 @@
 import { FC } from 'react';
+import ThreadListSection from './ThreadListSection';
+import Navbar from '@/common/components/ui/navbar';
 
 const HomePage: FC = () => {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <main id="main">
+        <section>
+          <div className="container mx-auto py-8">
+            <ThreadListSection />
+          </div>
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default HomePage;
