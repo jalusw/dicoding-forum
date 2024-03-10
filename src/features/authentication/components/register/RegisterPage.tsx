258fca744c0d9511e34929ca8b3ec3d55e650ad7
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-03-11 02:38:52 +0800

    add link to login page from register page

--- src/features/authentication/components/register/RegisterPage.tsx
+++ src/features/authentication/components/register/RegisterPage.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import RegisterHeader from './RegisterHeader';
 import RegisterForm from './RegisterForm';
+import { Link } from 'react-router-dom';
 
 const RegisterPage: FC = () => {
   return (
@@ -9,6 +10,9 @@ const RegisterPage: FC = () => {
         <RegisterHeader />
         <section className="mt-8">
           <RegisterForm />
+        </section>
+        <section className="mt-4">
+          <Link to="/login">Have an account already?</Link>
         </section>
       </div>
     </main>
