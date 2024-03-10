41c3964eb43a45bb46fa4729afb5ff33fcd88ae5
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-03-11 02:36:52 +0800

    add logout button & login action to navbar menu

--- src/common/components/ui/navbar.tsx
+++ src/common/components/ui/navbar.tsx
@@ -1,13 +1,17 @@
-import { useAppSelector } from '@/common/hooks';
+import { useAppDispatch, useAppSelector } from '@/common/hooks';
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from './button';
+import { logout } from '@/features/authentication/slices/authenticationSlice';
+import AuthenticatedWrapper from '../authentication/AuthenticatedWrapper';
+import GuestWrapper from '../authentication/GuestWrapper';
 
 const Navbar: FC = () => {
   return (
     <nav className="py-4 shadow-border">
       <div className="container  mx-auto flex justify-between">
         <NavbarBrand />
-        <NavbarLinks />
+        <NavbarMenu />
       </div>
     </nav>
   );
@@ -19,13 +23,33 @@ const NavbarBrand: FC = () => (
   </Link>
 );
 
-const NavbarLinks: FC = () => {
+const NavbarMenu: FC = () => {
   return (
-    <ul>
+    <ul className='flex items-center space-x-4'>
       <li>
         <Link to="/">Home</Link>
       </li>
+      <AuthenticatedWrapper>
+        <li>
+          <LogoutButton />
+        </li>
+      </AuthenticatedWrapper>
+      <GuestWrapper>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </GuestWrapper>
     </ul>
+  );
+};
+
+const LogoutButton: FC = () => {
+  const dispatch = useAppDispatch();
+  const onClick = () => dispatch(logout());
+  return (
+    <Button className="m-0" onClick={onClick} variant="link">
+      Logout
+    </Button>
   );
 };
 
