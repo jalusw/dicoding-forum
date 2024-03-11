35c144724cfa9abf187264936756f93d27524095
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-03-11 09:02:52 +0800

    formatting

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import { createRoot } from 'react-dom/client';
 import { persiststor, router, store } from '@/config';
 
 import '@/common/styles/index.css';
-import { PersistGate } from 'redux-persist/integration/react';
 
 const targetRootElement = document.querySelector('#root')!;
 const reactroot = createRoot(targetRootElement);
